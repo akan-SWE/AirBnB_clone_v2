423e95b11147d3a35850aecd5b6233e5800fea5a
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-06-26 15:26:09 +0100

    update: modified file to add and update class User attributes

--- models/user.py
+++ models/user.py
@@ -3,9 +3,21 @@
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes.
+
+    Attributes:
+        __tablename__: represents table name of the MySQL table to store users
+        email: represents a non-optional column containing a string(128 characters)
+        password: represents a non-optional column containing a string(128 characters)
+        first_name: represents optional column containing a string(128 characters)
+        last_name: represents optional column containing a string(128 characters)
+
+    """
+
+    __tablename__ = 'users'
+
+    email = Column(string(128), nullable=False)
+    password = Column(string(128), nullable=False)
+    first_name = Column(string(128), nullable=True)
+    last_name = Column(string(128), nullable=True)
