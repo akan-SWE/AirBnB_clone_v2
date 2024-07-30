84003f45d2d6eb7484a73ab0d3e1945b18139148
akanSWE <a.akanSWE@gamil.com>
2024-07-30 10:27:12 +0100

    Add Flask with with '/', '/hbnb' and '/c/<text>' routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module: 2-c_route
+
+This script starts a Flask web application.
+
+- Listens on 0.0.0.0, port 5000
+
+Routes:
+    '/' - Displays "Hello HBNB!"
+    '/hbnb' - Displays "HBNB"
+    '/c/<text>' - Displays "C" followed by the value of the text variable,
+                  with underscores replaced by spaces.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display 'C' followed by the value of the text variable
+    with underscore replaced by spaces
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
