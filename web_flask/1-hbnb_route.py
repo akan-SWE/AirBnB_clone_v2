f237fb2f31557a46b8206376a93135f2968c19f5
akanSWE <a.akanSWE@gamil.com>
2024-07-30 10:25:59 +0100

    Add Flask app with '/' and '/hbnb' routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module: 0-hello_route
+
+This script starts a Flask web application.
+
+- Listens on 0.0.0.0, port 5000
+
+Routes:
+    '/' - Displays "Hello HBNB!"
+    '/hbnb' - Displays "HBNB"
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
