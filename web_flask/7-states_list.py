3066d0879d3516967167d4373fe5dc8b43932ad4
akanSWE <a.akanSWE@gamil.com>
2024-08-09 12:48:39 +0100

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,18 +10,18 @@ Routes:
 Function:
     clean_session: Remove the current session after each request
 """
+
+from models.model_registry import mapped_classes
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def clean_session(var):
+def clean_session(exception):
     """
-    Remove the current session after each request
+    Remove the current session after each request.
     """
     storage.close()
 
@@ -29,10 +29,13 @@ def clean_session(var):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
-    Display a HTML page that lists all States objects
+    Display an HTML page that lists all States objects in ascending order
+    by name.
     """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    state_list = list(storage.all(mapped_classes['State']).values())
+    states_sorted = sorted(state_list, key=lambda s: s.name)
+
+    return render_template('7-states_list.html', states=states_sorted)
 
 
 if __name__ == '__main__':
